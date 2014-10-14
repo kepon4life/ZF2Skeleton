246c4ef9d1c7d5c255882f1726c50dad58cbeb9a
kepon4life <raphalinho7@hotmail.com>
2014-10-14 16:48:17 +0200

    First commit

---
+++ config/autoload/global.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbParams = array(
+    'database'  => 'zf2',
+    'username'  => 'developer',
+    'password'  => '123456',
+    'hostname'  => 'localhost'
+);
+ 
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($sm) use ($dbParams) {
+                $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
+                    'driver'    => 'pdo',
+                    'dsn'       => 'pgsql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
+                    'database'  => $dbParams['database'],
+                    'username'  => $dbParams['username'],
+                    'password'  => $dbParams['password'],
+                    'hostname'  => $dbParams['hostname'],
+                ));
+ 
+                $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler);
+                $adapter->injectProfilingStatementPrototype();
+                return $adapter;
+            },
+        ),
+    ),
+);
